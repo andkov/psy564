16d12d083ac3156cedd068c20dbc0053889a2e4b
Andrey Koval <koval.andrey@gmail.com>
2014-12-03 08:32:25 -0800

    update site

---
+++ _site/produceReport.R
@@ -0,0 +1,23 @@
+rm(list=ls(all=TRUE))
+########## Production of reports from .Rmd files ###
+
+pathFilesToBuild <- base::file.path("./5.Rmd")
+
+
+
+
+
+
+testit::assert("The knitr Rmd files should exist.", base::file.exists(pathFilesToBuild))
+# Build the report
+for( pathFile in pathFilesToBuild ) {
+  #   pathMd <- base::gsub(pattern=".Rmd$", replacement=".md", x=pathRmd)
+  rmarkdown::render(input = pathFile, 
+                    output_format=c(
+                      #                        "pdf_document"
+                      #                       ,"md_document"
+                                            "html_document"
+#                       "ioslides_presentation"
+                    ),
+                    clean=TRUE)
+}
