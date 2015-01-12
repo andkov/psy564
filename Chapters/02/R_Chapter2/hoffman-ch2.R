0338d461d63eea72d7ba9fb1a67d2a4a6a0c3747
Andrey Koval <koval.andrey@gmail.com>
2015-01-12 10:53:45 -0800

    hoffman chapter 2

--- Chapters/02/R_Chapter2/hoffman-ch2.R
+++ Chapters/02/R_Chapter2/hoffman-ch2.R
@@ -1,21 +1,31 @@
 # remove all elements for a clean start
 rm(list=ls(all=TRUE))
+# Supplementary Material for Longitudinal Analysis: Modeling Within-Person Fluctuation and Change 
+# Chapter 2: MPLUS Syntax and Output by Model
 
 ## @knitr LoadPackages
 require(sas7bdat)
+library(nlme)
+
 
 ## @knitr LoadData
 pathDir  <- getwd()
 pathCh2  <- file.path(pathDir,"Chapters/02/SAS_Chapter2/SAS_Chapter2.sas7bdat")
 dsL2   <- read.sas7bdat(pathCh2, debug=TRUE) 
 ds <- dsL2
-
 names(ds)
 str(ds)
 table(ds$sexMW, ds$demgroup)
 
 
-library(nlme)
-m0 <- gls(cognition ~ 1,data=ds,method="ML")
-ds$m0 <- predict(m0)
+## @knitr EmptyMeansModel
+m0 <- nlme::gls(cognition ~ 1, data=ds, method="ML")
 summary(m0)
+ds$m0 <- predict(m0)
+
+
+## @knitr AddingAge(0=85)
+ds$age85 <- ds$age - 85
+m1 <- nlme::gls(cognition ~ 1 + age85, data=ds, method="ML")
+summary(m1)
+ds$m1 <- predict(m1)
