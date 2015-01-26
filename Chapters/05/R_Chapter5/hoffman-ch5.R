d47bb50b7dd47be2e43fe53d4ca179b5c83bc892
Andrey Koval <koval.andrey@gmail.com>
2015-01-26 14:00:27 -0800

    basic report done

--- Chapters/05/R_Chapter5/hoffman-ch5.R
+++ Chapters/05/R_Chapter5/hoffman-ch5.R
@@ -3,143 +3,245 @@ rm(list=ls(all=TRUE))
 # Supplementary Material for Longitudinal Analysis: Modeling Within-Person Fluctuation and Change 
 # Chapter 2: MPLUS Syntax and Output by Model
 
+## @knitr InstallPackage
+# source("./Scripts/Utility/InstallPackages.R")
+
 ## @knitr LoadPackages
-require(sas7bdat)
-library(nlme)
-library(dplyr)
-library(reshape2)
-library(ggplot2)
-library(psych)
+require(sas7bdat) # for inputting data 
+library(dplyr) # for general data manipulation
+library(reshape2) # for data : wide <-> long
+library(psych) # data summary + etc
+library(ggplot2) # graphing
+library(nlme) # estimate mixed models | esp. gls()
+library(lme4) # estimate mixed models | esp. lmer()
+library(arm)  # process model objects
 
 ## @knitr LoadData
-pathDir  <- getwd()
-pathChapter  <- file.path(pathDir,"Chapters/05/SAS_Chapter5/SAS_Chapter5.sas7bdat")
-dsL5   <- read.sas7bdat(pathChapter, debug=TRUE) 
-ds <- data.frame(dsL5)
-names(ds)
-str(data.frame(ds))
-# basic tables
-head(ds)
-
-summary(ds)
-psych:summary(ds)
-
-table(ds$group)
-
-# stack data 
-dsLong <- reshape2::melt(ds,id.vars=c("PersonID","group"))
-# sort for visual inspection
-dsLong <- dsLong[order(dsLong$PersonID, dsLong$PersonID),]
-head(dsLong,12)
-# tweak the datatset
-dsLong$variable <- gsub(pattern="outcome", replacement='', x=dsLong$variable)
-head(dsLong,12)
-dsLong <- dplyr::select(dsLong, id=PersonID, group,time=variable, outcome=value)
-ds <- dsLong
+pathDir  <- getwd() # get working directory, e.i. residence of .Rproj file
+pathFile  <- file.path(pathDir,"Chapters/05/SAS_Chapter5/SAS_Chapter5.sas7bdat") # location of the file
+ds0   <- read.sas7bdat(pathFile, debug=TRUE) # import file 
+ds0 <- data.frame(ds0) # save as a data frame 
 
 
-ds$group <- ordered(ds$group,
-                               levels = c(1,2),
-                               labels = c("Control","Treatment"))
-ds$time <- ordered(ds$time,
-                    levels = c(1,2),
-                    labels = c("Pre-Test","Post-Test"))
+## @knitr BasicDescriptive
+class(ds0) # what class?
+dim(ds0)  # what dimensions?
+names(ds0) # what are column names?
+str(ds0) # what its structure?
+head(ds0) # what do first few lines look like?
+base::summary(ds0) # basic summary
+psych::describe(ds0) # summary by psych package
+table(ds0$wave)# one-way table
 
-str(ds)
 
+## @knitr StackData
+dsL <- ds0
 
+## @knitr TweakLong
+dsM <- dsL
 
-head(dsLong)
-ds <- dsLong
-head(ds)
-# Load graph themes
-source("Scripts/Graphs/graphThemes.R")
-# graph
-p <- ggplot2::ggplot(ds,aes(x=time,y=outcome))
-p <- p + geom_line(aes(group=id, color=factor(group)), alpha=1,
-                   position=position_jitter(w=0.0, h=0.0))
-p <- p + theme1
-p <- p + scale_x_discrete(limits=c(1,2), labels=c("Pre-Test","Post-Test"))
-p <- p + scale_y_continuous(limits=c(30,70), 
-                            breaks=seq(30,70, by=5))
-# p <- p + facet_grid(.~group)
-# p <- p + labs(list(
-#   title="How often did you attend worship last year?",
-#   x="Year of observation", y="Church attendance"))
+#### Basic Graphs  ####
+
+## @knitr GraphingData0
+p <- ggplot2::ggplot(dsM,aes(x=wave,y=outcome)) # map data dimension
+p <- p + geom_line(aes(group=PersonID)) # draw lines and map unit of measurement
+p
+
+## @knitr LoadGraphThemes
+baseSize <- 12 # set as the point of further reference
+theme1 <- ggplot2::theme_bw(base_size=baseSize) +
+  ggplot2::theme(title=ggplot2::element_text(colour="gray20",size = baseSize+1)) +
+  ggplot2::theme(axis.text=ggplot2::element_text(colour="gray40", size=baseSize-2)) +
+  ggplot2::theme(axis.title=ggplot2::element_text(colour="gray40")) +
+  ggplot2::theme(panel.border = ggplot2::element_rect(colour="gray80")) +
+  ggplot2::theme(axis.ticks.length = grid::unit(0, "cm")) +
+  ggplot2::theme(text = element_text(size =baseSize+7)) 
+
+## @knitr GraphingData1
+p <- ggplot2::ggplot(dsM,aes(x=wave,y=outcome))
+p <- p + geom_line(aes(group=PersonID)) # draw lines
+p <- p + theme1 
+p <- p + scale_x_continuous(limits=c(1,4), breaks=c(1:4)) # X axis
+p <- p + scale_y_continuous(limits=c(5,25), 
+                            breaks=seq(5,25, by=5)) # Y axis
+p <- p + labs(list(
+  title="Does outcome change over time?", # main title
+  x="Wave of measurement", y="Performance on the outcome")) # axes titles
 p
 
 
+## @knitr GraphingData2
+p <- ggplot2::ggplot(dsM,aes(x=wave,y=outcome))
+p <- p + geom_line(aes(group=PersonID)) 
+p <- p + geom_point(size=6, shape=21, fill="purple", color="black", alpha=.5)
+p <- p + theme1 
+p <- p + scale_x_continuous(limits=c(1,4), breaks=c(1:4)) # X axis
+p <- p + scale_y_continuous(limits=c(5,25), 
+                            breaks=seq(5,25, by=5)) # Y axis
+p <- p + labs(list(
+  title="Does outcome change over time?", # main title
+  x="Wave of measurement", y="Performance on the outcome")) # axes titles
+p
+
+## @knitr dummyChunk
+####  MODEL 3.1  ####
+
+## @knitr RunM3_1
+m3.1 <- nlme::gls(outcome ~ 1, data=dsM, method="ML") # create model object
+dsM$m3.1 <- predict(m3.1) # stores values predicted by the model
+summary(m3.1) # print a bit more info
+# str(summary(m3.1)) # to inspect object directly
 
 
 
-## @knitr EmptyMeansModel
-m3.1 <- nlme::gls(outcome ~ 1, data=ds, method="ML", correlation=corSymm(form = ~ 1|id))
-summary(m3.1)
-str(summary(m3.1))
-ds$m3.1 <- predict(m3.1)
-
-var(ds$outcome - ds$m3.1)
-
-
-model <- m3.1
-logLik<- summary(model)$logLik
-deviance<- -2*logLik
-AIC<- AIC(model)
-BIC<- BIC(model)
-df.resid<- NA
-N<- summary(model)$dims$N
-p<- summary(model)$dims$p
-ids<- length(unique(ds$PersonID))
-df.resid<- N-p
-mInfo<- data.frame("logLik" = logLik, 
-                   "deviance"= deviance, 
-                   "AIC" = AIC, "BIC" = BIC,
-                   "df.resid" = df.resid, "N" = N, 
-                   "p" = p, "ids" = ids)
-t<- t(mInfo)
-rownames(t)<-colnames(mInfo)
-dsmInfo<- data.frame(new=t)
-colnames(dsmInfo) <- c("m3.1")
-# dsmInfo$Coefficient <- rownames(dsmInfo)
-mo3.1 <- dsmInfo
-mo3.1$Coefficient <- rownames(mo3.1)
-summary(m3.1)
-print(mo3.1)
+## @knitr InspectM3_1
+model  <- m3.1 # rename object for generic use
+logLik <- summary(model)$logLik # extract log likelihood
+deviance <- -2*logLik # extract deviance
+AIC <- AIC(model) # extract Akaike information criterion
+BIC <- BIC(model) # extract Bayesian information criterion
+df.resid <- NA # empty slot for later use
+N <- summary(model)$dims$N  # Number of distinct data points
+p <- summary(model)$dims$p  # Number of estimated parameters
+ids <- length(unique(dsM$PersonID)) # Number of unique units
+df.resid <- N-p # residual degrees of freedom
+mInfo <- data.frame("logLik" = logLik,   # collect model information indo a dataframe
+                    "deviance"= deviance, 
+                    "AIC" = AIC, "BIC" = BIC,
+                    "df.resid" = df.resid, "N" = N, 
+                    "p" = p, "ids" = ids)
+t<- t(mInfo) # transpose
+rownames(t)<-colnames(mInfo) # rename rows
+mInfo<- data.frame(new=t) # turn into a dataframe
+colnames(mInfo) <- c("m3.1") # rename columns
+mi3.1 <- mInfo # save (m)odel (o)utput of model (3.1)
+mi3.1$Coefficient <- rownames(mi3.1) # create a column with the name of each index
+m3.1 #  model results
+mi3.1 #  model information
+head(dsM) # visual check
 
 
+## @knitr GraphM3_1
+p <- ggplot2::ggplot(dsM,aes(x=wave, y=outcome))
+p <- p + geom_line(aes(group=PersonID), color="firebrick", alpha=.5)  # individual trajectories
+p <- p + geom_point(size=3, shape=21, fill=NA, color="black", alpha=.4) # cross-section data points
+p <- p + geom_line(aes(y=m3.1, group=PersonID), color="royalblue3", size=3, alpha=.05) # modelled data
+p <- p + theme1 
+p <- p + scale_x_continuous(limits=c(1,4), breaks=c(1:4)) # X axis
+p <- p + scale_y_continuous(limits=c(5,25), 
+                            breaks=seq(5,25, by=1)) # Y axis
+p <- p + labs(list(
+  title="Does outcome change over time?", # main title
+  x="Wave of measurement", y="Performance on the outcome")) # axes titles
+p
 
+## @knitr dummyChunk
+####  MODEL 5.1  ####
 
-## @knitr EmptyMeansModel
-m3.2 <- lme4::lmer(outcome ~ 1 + (1|id), data=ds, REML=FALSE)
-summary(m3.2)
-ds$m3.2 <- predict(m3.2)
-var(ds$outcome - ds$m3.2)
+## @knitr RunM5_1
+m5.1 <- lme4::lmer(outcome ~ 1 + (1 | PersonID), data=dsM, REML=FALSE)# create model object
+dsM$m5.1 <- predict(m5.1) # stores values predicted by the model
+display(m5.1) # tidy results
 
-model <- m3.2
+## @knitr InspectM5_1
+model <- m5.1
 mInfo<-summary(model)$AICtab
 mInfo["N"]<- model@devcomp$dims["N"] # number of datapoints, verify
 mInfo["p"]<- model@devcomp$dims["p"] # number of estimated parameters, verify
 mInfo["ids"]<- (summary(model))$ngrps # number of units on level-2, here: individuals
 # mInfo<- c(mInfo, "modelName"=modelName)
-dsmInfo<-data.frame(mInfo)
-dsmInfo<- plyr::rename(dsmInfo,replace= c("mInfo"="m3.2"))
-dsmInfo$Coefficient <- rownames(dsmInfo)
-mo3.2 <- dsmInfo # model information 
+mInfo<-data.frame(mInfo) # turn into a dataframe
+mInfo<- plyr::rename(mInfo,replace= c("mInfo"="m5.1")) # rename variables
+mInfo$Coefficient <- rownames(mInfo) # save index names as a column
+mi5.1 <- mInfo # create model information  object
+display(m5.1)
+mi5.1 #  model information
+head(dsM) # visual check
 
-print(mo3.2)
-print(mo3.1) 
+## @knitr GraphM5_1
+p <- ggplot2::ggplot(dsM,aes(x=wave, y=outcome))
+p <- p + geom_line(aes(group=PersonID), color="firebrick", alpha=.2)  # individual trajectories
+p <- p + geom_point(size=3, shape=21, fill=NA, color="black", alpha=.6) # cross-section data points
+p <- p + geom_line(aes(y=m5.1, group=PersonID), color="royalblue3", alpha=.7) # modelled data
+p <- p + theme1 
+p <- p + scale_x_continuous(limits=c(1,4), breaks=c(1:4)) # X axis
+p <- p + scale_y_continuous(limits=c(5,25), 
+                            breaks=seq(5,25, by=1)) # Y axis
+p <- p + labs(list(
+  title="Does outcome change over time?", # main title
+  x="Wave of measurement", y="Performance on the outcome")) # axes titles
+p
 
+## @knitr dummyChunk
+####  MODEL 5.3  ####
 
+## @knitr RunM5_3
+m5.3 <- lme4::lmer(outcome ~ 1 + wave + (1 | PersonID), data=dsM, REML=FALSE)# create model object
+dsM$m5.3 <- predict(m5.3) # stores values predicted by the model
+display(m5.3) # tidy results
 
+## @knitr InspectM5_3
+model <- m5.3
+mInfo<-summary(model)$AICtab
+mInfo["N"]<- model@devcomp$dims["N"] # number of datapoints, verify
+mInfo["p"]<- model@devcomp$dims["p"] # number of estimated parameters, verify
+mInfo["ids"]<- (summary(model))$ngrps # number of units on level-2, here: individuals
+# mInfo<- c(mInfo, "modelName"=modelName)
+mInfo<-data.frame(mInfo) # turn into a dataframe
+mInfo<- plyr::rename(mInfo,replace= c("mInfo"="m5.3")) # rename variables
+mInfo$Coefficient <- rownames(mInfo) # save index names as a column
+mi5.3 <- mInfo # create model information  object
+display(m5.3)
+mi5.3 #  model information
+head(dsM) # visual check
 
+## @knitr GraphM5_3
+p <- ggplot2::ggplot(dsM,aes(x=wave, y=outcome))
+p <- p + geom_line(aes(group=PersonID), color="firebrick", alpha=.2)  # individual trajectories
+p <- p + geom_point(size=3, shape=21, fill=NA, color="black", alpha=.6) # cross-section data points
+p <- p + geom_line(aes(y=m5.3, group=PersonID), color="royalblue3", alpha=.7) # modelled data
+p <- p + theme1 
+p <- p + scale_x_continuous(limits=c(1,4), breaks=c(1:4)) # X axis
+p <- p + scale_y_continuous(limits=c(5,25), 
+                            breaks=seq(5,25, by=1)) # Y axis
+p <- p + labs(list(
+  title="Does outcome change over time?", # main title
+  x="Wave of measurement", y="Performance on the outcome")) # axes titles
+p
 
+## @knitr dummyChunk
+####  MODEL 5.5  ####
 
+## @knitr RunM5_5
+m5.5 <- lme4::lmer(outcome ~ 1 + wave + (1 + wave | PersonID), data=dsM, REML=FALSE)# create model object
+dsM$m5.5 <- predict(m5.5) # stores values predicted by the model
+display(m5.5) # tidy results
 
+## @knitr InspectM5_5
+model <- m5.5
+mInfo<-summary(model)$AICtab
+mInfo["N"]<- model@devcomp$dims["N"] # number of datapoints, verify
+mInfo["p"]<- model@devcomp$dims["p"] # number of estimated parameters, verify
+mInfo["ids"]<- (summary(model))$ngrps # number of units on level-2, here: individuals
+# mInfo<- c(mInfo, "modelName"=modelName)
+mInfo<-data.frame(mInfo) # turn into a dataframe
+mInfo<- plyr::rename(mInfo,replace= c("mInfo"="m5.5")) # rename variables
+mInfo$Coefficient <- rownames(mInfo) # save index names as a column
+mi5.5 <- mInfo # create model information  object
+display(m5.5)
+mi5.5 #  model information
+head(dsM) # visual check
 
-
-
-
-
-
-
-
+## @knitr GraphM5_5
+p <- ggplot2::ggplot(dsM,aes(x=wave, y=outcome))
+p <- p + geom_line(aes(group=PersonID), color="firebrick", alpha=.2)  # individual trajectories
+p <- p + geom_point(size=3, shape=21, fill=NA, color="black", alpha=.6) # cross-section data points
+p <- p + geom_line(aes(y=m5.5, group=PersonID), color="royalblue3", alpha=.7) # modelled data
+p <- p + theme1 
+p <- p + scale_x_continuous(limits=c(1,4), breaks=c(1:4)) # X axis
+p <- p + scale_y_continuous(limits=c(5,25), 
+                            breaks=seq(5,25, by=1)) # Y axis
+p <- p + labs(list(
+  title="Does outcome change over time?", # main title
+  x="Wave of measurement", y="Performance on the outcome")) # axes titles
+p
--- Scripts/Utility/InstallPackages.R
+++ Scripts/Utility/InstallPackages.R
@@ -4,35 +4,40 @@
 rm(list=ls(all=TRUE)) #Clear the memory for any variables set from any previous runs.
 
 packagesToInstall <- c(
-  "colorspace" #Explicit control over the HCL color scheme
+   "arm" # for working with model objects, by Gelman & Hill
+  , "colorspace" #Explicit control over the HCL color scheme
   , "devtools" #package development
+  , "dplyr" # for general data manipulation
   , "ggplot2" #Graphing
   , "ggthemes" #Extra themes, scales and geoms for ggplot
   , "ggmap" #Maps & graphics, based on ggplot
   , "googleVis" #JavaScript-based visualizations, like scrollable tables
-  , "grid" #The underlying framework for the graphics used in this pacakge
+  , "grid" #The underlying framework for the graphics 
+  , "gridExtra" #for table FERE graphs
   , "gridBase" #Additional grid functions
-  , "knitr" #For reporting
+  , "knitr" #For creating dynamic reports
+  , "lme4" # used for Random Coefficient Modeling  
   , "lubridate" #Consistent/convienent function signatures for manipulating dates
+  , "minqa"
+  , "nlme" # used for Fixed Effect modeling  
   , "plyr" #Important for most of our data manipulation
+  , "psych" # data summary + other useful function
   , "RColorBrewer" #Explicit control over the Color Brewer colors.  See http://colorbrewer2.org/
-  , "reshape2" #Data manipulation not covered in plyr
+  , "reshape2" #Data manipulation not covered in plyr,  wide <-> long
+  , "RJSONIO" # for processing jason files
   , "roxygen2" #Creates documentation Rd file from (well-formed) comments
+  , "RODBC"
+  , "sas7bdat" # Imports SAS formatted data files
+  , "stringr"
   , "testit" #has the useful `assert()` function
   , "testthat" #Heavier testing framework that's good for package development
-  , "sas7bdat" # Imports SAS formatted data files
-  , "RJSONIO"
-  , "dplyr"
-  , "reshape2"
-  , "dplyr"
-  , "lme4" # used for Random Coefficient Modeling
-  , "minqa"
-  , "nlme" # used for Fixed effect modelin
-  , "stringr"
-  , "yaml"
-  , "gridExtra"
-  , "RODBC"
+  , "yaml" #for gh-pages production
+
 ) 
+
+
+
+
 
 for( packageName in packagesToInstall ) {
   available <- require(packageName, character.only=TRUE) #Loads the packages, and indicates if it's available
